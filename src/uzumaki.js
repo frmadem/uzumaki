0341381d01279801615aff96ac687fcbb160ded7
frmadem <frmadem@frmadem-VirtualBox.(none)>
2015-08-02 17:52:20 +0200

    Purge of innecesary code for inChildProcess

--- src/uzumaki.js
+++ src/uzumaki.js
@@ -1,24 +1,24 @@
 var Registry = require('./registry.js').registry();
 
-var F_IN_CHILD_PROCESS = false;
-
-module.exports.inChildProcess = function(set){
-
-	if(set === undefined){
-		return F_IN_CHILD_PROCESS;
-	}
-	else{
-
-		F_IN_CHILD_PROCESS = set;
-
-		if(F_IN_CHILD_PROCESS){
-
-			Registry = require('./registry_child_process.js').registry();		
-
-		}
-	}
-
-};
+//var F_IN_CHILD_PROCESS = false;
+//
+//module.exports.inChildProcess = function(set){
+//
+//	if(set === undefined){
+//		return F_IN_CHILD_PROCESS;
+//	}
+//	else{
+//
+//		F_IN_CHILD_PROCESS = set;
+//
+//		if(F_IN_CHILD_PROCESS){
+//
+//			Registry = require('./registry_child_process.js').registry();		
+//
+//		}
+//	}
+//
+//};
 
 module.exports.spawn = function(actor, receive){
 
